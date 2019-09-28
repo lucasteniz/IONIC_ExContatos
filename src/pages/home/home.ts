f77153604b5ee0dba24b8375177e7a71e2dec797
arqdsis <arqdsis@MOOLIi0317.animaedu.intranet>
2019-09-28 15:46:59 -0300

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,32 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  contatos = [
+    {
+    id: '1',
+    nome: 'Lucas',
+    telefone: '1111111111',
+    email: 'lucas@gmail.com'
+    },
+    {
+    id: '2',
+    nome: 'Gabriel',
+    telefone: '1111114441',
+    email: 'gabriel@gmail.com'
+    },
+    {
+    id: '3',
+    nome: 'Tatiana',
+    telefone: '444444444',
+    email: 'tatiana@gmail.com'
+    }
+  ]
   constructor(public navCtrl: NavController) {
 
+  }
+
+  onItemClick(contato){
+    console.log('item.click', contato);
   }
 
 }
